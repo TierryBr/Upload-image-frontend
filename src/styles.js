d387f584b9c5339e28feadf1fba6a91a20baec46
TierryBr <tierri-2011@hotmail.com>
2020-07-09 15:08:19 -0300

    Frontend da aplicação

---
+++ src/styles.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  margin-top: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+export const Content = styled.div`
+    width: 100%;
+    max-width: 400px;
+    margin: 30px;
+    background: #fff;
+    border-radius: 4px;
+    padding: 20px;
+`;
+
+export const ContainerName = styled.div`
+    font-size: 17px;
+    color: #fff;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 100px
+  
+`;
